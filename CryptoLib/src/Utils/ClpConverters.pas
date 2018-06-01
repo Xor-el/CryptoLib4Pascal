6c69e41215aabf5a3c4d9f2c327f61d0756976ef
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2018-06-01 20:37:36 +0100

    minor warning fix.

--- CryptoLib/src/Utils/ClpConverters.pas
+++ CryptoLib/src/Utils/ClpConverters.pas
@@ -478,7 +478,11 @@ begin
     raise EArgumentNilCryptoLibException.CreateRes(@SEncodingInstanceNil);
   end;
 
+{$IFDEF FPC}
+  result := a_encoding.GetBytes(UnicodeString(a_in));
+{$ELSE}
   result := a_encoding.GetBytes(a_in);
+{$ENDIF FPC}
 end;
 
 class function TConverters.SplitString(const S: String; Delimiter: Char)
