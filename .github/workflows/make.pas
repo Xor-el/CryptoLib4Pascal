e0e2391df6cfa1ee7581efb4f9c6104d4a5506d4
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-01-18 17:23:55 +0200

    fix github-actions

--- .github/workflows/make.pas
+++ .github/workflows/make.pas
@@ -176,10 +176,10 @@ type
   end;
 
 begin
-  if ParamCount <> 0 then
-    case ParamStr(1) of
-      'build': BuildAll;
-      else
-        OutLog(audit, 'Nothing!');
-    end;
+  try
+    BuildAll
+  except
+    on E: Exception do
+      Writeln(E.ClassName, #9, E.Message);
+  end;
 end.
