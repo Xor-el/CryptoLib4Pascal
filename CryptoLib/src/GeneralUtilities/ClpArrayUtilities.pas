85c307cea3d63488e3d1d750410a5820e98e1f77
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2026-03-11 02:12:18 +0100

    some code cleanup

--- CryptoLib/src/GeneralUtilities/ClpArrayUtilities.pas
+++ CryptoLib/src/GeneralUtilities/ClpArrayUtilities.pas
@@ -24,7 +24,6 @@ interface
 uses
   SysUtils,
   Math,
-  Generics.Defaults,
   ClpCryptoLibTypes;
 
 resourcestring
@@ -32,14 +31,15 @@ resourcestring
 
 type
   TArrayUtilities = class sealed(TObject)
-
   strict private
     class function GetLength(AFrom, ATo: Int32): Int32; static; inline;
 
+    /// <summary>
+    /// Core hash computation shared by all GetArrayHashCode overloads.
+    /// Processes ACount bytes starting at AData.
+    /// </summary>
+    class function HashCore(AData: PByte; ACount: Int32): Int32; static;
   public
-
-   (* class function AreEqual<T>(const A, B: TCryptoLibGenericArray<T>;
-      const AComparer: IEqualityComparer<T> = nil): Boolean; overload; static; *)
 
     class function AreEqual(const A, B: TCryptoLibByteArray): Boolean; overload; static;
     class function AreEqual(const A, B: TCryptoLibInt32Array): Boolean; overload; static;
@@ -73,15 +73,32 @@ type
     class function Append<T>(const A: TCryptoLibGenericArray<T>; const B: T)
       : TCryptoLibGenericArray<T>; static;
 
+    /// <summary>
+    /// Returns a new array of ANewLength, copying min(ANewLength, Length(AData))
+    /// elements from AData. Returns nil if AData is nil.
+    /// </summary>
     class function CopyOf<T>(const AData: TCryptoLibGenericArray<T>; ANewLength: Int32)
       : TCryptoLibGenericArray<T>; static;
 
+    /// <summary>
+    /// Returns a new array containing AData[AFrom..ATo-1].
+    /// Returns nil if AData is nil.
+    /// </summary>
     class function CopyOfRange<T>(const AData: TCryptoLibGenericArray<T>;
       AFrom, ATo: Int32): TCryptoLibGenericArray<T>; static;
 
+    /// <summary>
+    /// Constant-time comparison of two byte arrays.
+    /// Both length and content comparisons are constant-time to avoid
+    /// leaking information through timing side channels.
+    /// </summary>
     class function FixedTimeEquals(const AAr1, AAr2: TCryptoLibByteArray)
       : Boolean; overload; static;
 
+    /// <summary>
+    /// Constant-time comparison of ALen bytes from AA[AOff1] and AB[AOff2].
+    /// Caller must ensure offsets and length are within bounds.
+    /// </summary>
     class function FixedTimeEquals(ALen: Int32; const AA: TCryptoLibByteArray;
       AOff1: Int32; const AB: TCryptoLibByteArray; AOff2: Int32)
       : Boolean; overload; static;
@@ -89,15 +106,17 @@ type
     class procedure Fill<T>(ABuf: TCryptoLibGenericArray<T>; AFrom, ATo: Int32;
       const AFiller: T); static;
 
-    class function Clone<T>(const AData: TCryptoLibGenericArray<T>; const ACloneFunc: TFunc<T, T>): TCryptoLibGenericArray<T>; static;
+    /// <summary>
+    /// Deep-clone an array using ACloneFunc. Returns nil if AData is nil.
+    /// If ACloneFunc raises mid-way, already-cloned objects are freed to prevent leaks.
+    /// </summary>
+    class function Clone<T>(const AData: TCryptoLibGenericArray<T>;
+      const ACloneFunc: TFunc<T, T>): TCryptoLibGenericArray<T>; static;
 
     class function NoZeroes(const AData: TCryptoLibByteArray): Boolean; static;
 
-   (* class function Contains<T>(const AData: TCryptoLibGenericArray<T>;
-      const AValue: T; const AComparer: IEqualityComparer<T> = nil): Boolean; overload; static; *)
-
     class function Contains(const AData: TCryptoLibInt32Array;
-      const AValue: Int32): Boolean; overload; static;
+      const AValue: Int32): Boolean; static;
 
     class function Map<T, TResult>(const AData: TCryptoLibGenericArray<T>;
       const AFunc: TFunc<T, TResult>): TCryptoLibGenericArray<TResult>; static;
@@ -105,16 +124,12 @@ type
     class function ToString<T>(const AData: TCryptoLibGenericArray<T>;
       const AConverter: TFunc<T, String>): String; reintroduce; overload; static;
 
-    /// <summary>
-    /// Reverse array elements in place.
-    /// </summary>
+    /// <summary>Reverse array elements in place.</summary>
     class procedure ReverseInPlace<T>(var AArray: TCryptoLibGenericArray<T>); overload; static;
-    /// <summary>
-    /// Reverse the range [AFromIndex, AToIndex) in place.
-    /// </summary>
+
+    /// <summary>Reverse the range [AFromIndex, AToIndex) in place.</summary>
     class procedure ReverseInPlace<T>(var AArray: TCryptoLibGenericArray<T>;
       AFromIndex, AToIndex: Int32); overload; static;
-
   end;
 
 implementation
@@ -122,48 +137,24 @@ implementation
 { TArrayUtilities }
 
 class function TArrayUtilities.GetLength(AFrom, ATo: Int32): Int32;
-var
-  LNewLength: Int32;
 begin
-  LNewLength := ATo - AFrom;
-  if LNewLength < 0 then
+  Result := ATo - AFrom;
+  if Result < 0 then
     raise EArgumentCryptoLibException.CreateResFmt(@SInvalidLength, [AFrom, ATo]);
-  Result := LNewLength;
 end;
 
-class function TArrayUtilities.NoZeroes(const AData: TCryptoLibByteArray): Boolean;
+class function TArrayUtilities.HashCore(AData: PByte; ACount: Int32): Int32;
 var
-  I: Int32;
+  LI, LHc: Int32;
 begin
-  Result := True;
-  for I := System.Low(AData) to System.High(AData) do
+  LHc := ACount + 1;
+  for LI := ACount - 1 downto 0 do
   begin
-    if AData[I] = 0 then
-    begin
-      Result := False;
-      Exit;
-    end;
+    LHc := LHc * 257;
+    LHc := LHc xor AData[LI];
   end;
+  Result := LHc;
 end;
-
-(*
-class function TArrayUtilities.AreEqual<T>(const A, B: TCryptoLibGenericArray<T>;
-  const AComparer: IEqualityComparer<T>): Boolean;
-var
-  I: Int32;
-  LComparer: IEqualityComparer<T>;
-begin
-  if System.Length(A) <> System.Length(B) then
-    Exit(False);
-  if AComparer = nil then
-    LComparer := TEqualityComparer<T>.Default
-  else
-    LComparer := AComparer;
-  for I := System.Low(A) to System.High(A) do
-    if not LComparer.Equals(A[I], B[I]) then
-      Exit(False);
-  Result := True;
-end; *)
 
 class function TArrayUtilities.AreEqual(const A, B: TCryptoLibByteArray): Boolean;
 var
@@ -192,38 +183,46 @@ end;
 class function TArrayUtilities.Concatenate<T>(const A, B: TCryptoLibGenericArray<T>)
   : TCryptoLibGenericArray<T>;
 var
-  I, LLenA, LLenB: Int32;
+  LI, LLenA, LLenB: Int32;
 begin
   LLenA := System.Length(A);
   LLenB := System.Length(B);
   System.SetLength(Result, LLenA + LLenB);
-  for I := 0 to LLenA - 1 do
-    Result[I] := A[I];
-  for I := 0 to LLenB - 1 do
-    Result[LLenA + I] := B[I];
+  for LI := 0 to LLenA - 1 do
+    Result[LI] := A[LI];
+  for LI := 0 to LLenB - 1 do
+    Result[LLenA + LI] := B[LI];
 end;
 
 class function TArrayUtilities.AreAllZeroes(const ABuf: TCryptoLibByteArray;
   AOff, ALen: Int32): Boolean;
 var
   LBits: UInt32;
-  I: Int32;
+  LI: Int32;
 begin
   LBits := 0;
-  for I := 0 to System.Pred(ALen) do
-    LBits := LBits or UInt32(ABuf[AOff + I]);
+  for LI := 0 to System.Pred(ALen) do
+    LBits := LBits or UInt32(ABuf[AOff + LI]);
   Result := LBits = 0;
 end;
 
 class function TArrayUtilities.FixedTimeEquals(const AAr1,
   AAr2: TCryptoLibByteArray): Boolean;
+var
+  LLenA, LLenB, LLen, LI: Int32;
+  LDiff: UInt32;
 begin
-  if System.Length(AAr1) <> System.Length(AAr2) then
-  begin
-    Result := False;
-    Exit;
-  end;
-  Result := FixedTimeEquals(System.Length(AAr1), AAr1, 0, AAr2, 0);
+  LLenA := System.Length(AAr1);
+  LLenB := System.Length(AAr2);
+  // Accumulate length mismatch without branching
+  LDiff := UInt32(LLenA xor LLenB);
+  // Compare up to the shorter length to stay in bounds
+  LLen := LLenA;
+  if LLenB < LLen then
+    LLen := LLenB;
+  for LI := 0 to LLen - 1 do
+    LDiff := LDiff or UInt32(AAr1[LI] xor AAr2[LI]);
+  Result := LDiff = 0;
 end;
 
 class function TArrayUtilities.FixedTimeEquals(ALen: Int32;
@@ -235,169 +234,107 @@ var
 begin
   LDiff := 0;
   for LI := 0 to System.Pred(ALen) do
-  begin
-    LDiff := LDiff or (UInt32(AA[AOff1 + LI] xor AB[AOff2 + LI]));
-  end;
+    LDiff := LDiff or UInt32(AA[AOff1 + LI] xor AB[AOff2 + LI]);
   Result := LDiff = 0;
 end;
 
 class function TArrayUtilities.CopyOf<T>(const AData: TCryptoLibGenericArray<T>;
   ANewLength: Int32): TCryptoLibGenericArray<T>;
 var
-  I, LCount: Int32;
+  LI, LCount: Int32;
 begin
   if AData = nil then
     Exit(nil);
   System.SetLength(Result, ANewLength);
   LCount := Min(ANewLength, System.Length(AData));
-  for I := 0 to LCount - 1 do
-    Result[I] := AData[I];
+  for LI := 0 to LCount - 1 do
+    Result[LI] := AData[LI];
 end;
 
 class function TArrayUtilities.CopyOfRange<T>(const AData: TCryptoLibGenericArray<T>;
   AFrom, ATo: Int32): TCryptoLibGenericArray<T>;
 var
-  I, LNewLength, LCount: Int32;
+  LI, LNewLength, LCount: Int32;
 begin
   if AData = nil then
     Exit(nil);
   LNewLength := GetLength(AFrom, ATo);
   System.SetLength(Result, LNewLength);
   LCount := Min(LNewLength, System.Length(AData) - AFrom);
-  for I := 0 to LCount - 1 do
-    Result[I] := AData[AFrom + I];
+  for LI := 0 to LCount - 1 do
+    Result[LI] := AData[AFrom + LI];
 end;
 
 class procedure TArrayUtilities.Fill<T>(ABuf: TCryptoLibGenericArray<T>;
   AFrom, ATo: Int32; const AFiller: T);
 var
-  I: Int32;
+  LI: Int32;
 begin
   if ABuf = nil then
     Exit;
-  for I := AFrom to ATo - 1 do
-    ABuf[I] := AFiller;
+  for LI := AFrom to ATo - 1 do
+    ABuf[LI] := AFiller;
 end;
 
 class function TArrayUtilities.GetArrayHashCode(const AData: TCryptoLibByteArray): Int32;
-var
-  I, LHc: Int32;
 begin
   if AData = nil then
     Exit(0);
-  I := System.Length(AData);
-  LHc := I + 1;
-  System.Dec(I);
-  while I >= 0 do
-  begin
-    LHc := LHc * 257;
-    LHc := LHc xor AData[I];
-    System.Dec(I);
-  end;
-  Result := LHc;
+  Result := HashCore(@AData[0], System.Length(AData));
 end;
 
 class function TArrayUtilities.GetArrayHashCode(const AData: TCryptoLibByteArray;
   AOff, ALen: Int32): Int32;
-var
-  I, LHc: Int32;
 begin
   if AData = nil then
     Exit(0);
-  I := ALen;
-  LHc := I + 1;
-  System.Dec(I);
-  while I >= 0 do
-  begin
-    LHc := LHc * 257;
-    LHc := LHc xor AData[AOff + I];
-    System.Dec(I);
-  end;
-  Result := LHc;
+  Result := HashCore(@AData[AOff], ALen);
 end;
 
 class function TArrayUtilities.GetArrayHashCode(const AData: TCryptoLibInt32Array): Int32;
-var
-  I, LHc: Int32;
 begin
   if AData = nil then
     Exit(0);
-  I := System.Length(AData);
-  LHc := I + 1;
-  System.Dec(I);
-  while I >= 0 do
-  begin
-    LHc := LHc * 257;
-    LHc := LHc xor AData[I];
-    System.Dec(I);
-  end;
-  Result := LHc;
+  Result := HashCore(PByte(@AData[0]), System.Length(AData) * System.SizeOf(Int32));
 end;
 
 class function TArrayUtilities.GetArrayHashCode(const AData: TCryptoLibUInt32Array): Int32;
-var
-  I, LHc: Int32;
 begin
   if AData = nil then
     Exit(0);
-  I := System.Length(AData);
-  LHc := I + 1;
-  System.Dec(I);
-  while I >= 0 do
-  begin
-    LHc := LHc * 257;
-    LHc := LHc xor Int32(AData[I]);
-    System.Dec(I);
-  end;
-  Result := LHc;
+  Result := HashCore(PByte(@AData[0]), System.Length(AData) * System.SizeOf(UInt32));
 end;
 
 class function TArrayUtilities.GetArrayHashCode(const AData: TCryptoLibUInt32Array;
   AOff, ALen: Int32): Int32;
-var
-  I, LHc: Int32;
 begin
   if AData = nil then
     Exit(0);
-  I := ALen;
-  LHc := I + 1;
-  System.Dec(I);
-  while I >= 0 do
-  begin
-    LHc := LHc * 257;
-    LHc := LHc xor Int32(AData[AOff + I]);
-    System.Dec(I);
-  end;
-  Result := LHc;
+  Result := HashCore(PByte(@AData[AOff]), ALen * System.SizeOf(UInt32));
 end;
 
 class function TArrayUtilities.GetArrayHashCode(const AData: TCryptoLibUInt64Array;
   AOff, ALen: Int32): Int32;
-var
-  I, LHc: Int32;
-  LDi: UInt64;
 begin
   if AData = nil then
     Exit(0);
-  I := ALen;
-  LHc := I + 1;
-  System.Dec(I);
-  while I >= 0 do
-  begin
-    LDi := AData[AOff + I];
-    LHc := LHc * 257;
-    LHc := LHc xor Int32(LDi);
-    LHc := LHc * 257;
-    LHc := LHc xor Int32(LDi shr 32);
-    System.Dec(I);
-  end;
-  Result := LHc;
+  Result := HashCore(PByte(@AData[AOff]), ALen * System.SizeOf(UInt64));
+end;
+
+class function TArrayUtilities.NoZeroes(const AData: TCryptoLibByteArray): Boolean;
+var
+  LI: Int32;
+begin
+  for LI := System.Low(AData) to System.High(AData) do
+    if AData[LI] = 0 then
+      Exit(False);
+  Result := True;
 end;
 
 class function TArrayUtilities.Prepend<T>(const A: TCryptoLibGenericArray<T>; const B: T)
   : TCryptoLibGenericArray<T>;
 var
-  I, LLength: Int32;
+  LI, LLength: Int32;
 begin
   if A = nil then
   begin
@@ -408,14 +345,14 @@ begin
   LLength := System.Length(A);
   System.SetLength(Result, LLength + 1);
   Result[0] := B;
-  for I := 0 to LLength - 1 do
-    Result[I + 1] := A[I];
+  for LI := 0 to LLength - 1 do
+    Result[LI + 1] := A[LI];
 end;
 
 class function TArrayUtilities.Append<T>(const A: TCryptoLibGenericArray<T>; const B: T)
   : TCryptoLibGenericArray<T>;
 var
-  I, LLength: Int32;
+  LI, LLength: Int32;
 begin
   if A = nil then
   begin
@@ -425,50 +362,50 @@ begin
   end;
   LLength := System.Length(A);
   System.SetLength(Result, LLength + 1);
-  for I := 0 to LLength - 1 do
-    Result[I] := A[I];
+  for LI := 0 to LLength - 1 do
+    Result[LI] := A[LI];
   Result[LLength] := B;
 end;
 
-class function TArrayUtilities.Clone<T>(const AData: TCryptoLibGenericArray<T>; const ACloneFunc: TFunc<T, T>): TCryptoLibGenericArray<T>;
+class function TArrayUtilities.Clone<T>(const AData: TCryptoLibGenericArray<T>;
+  const ACloneFunc: TFunc<T, T>): TCryptoLibGenericArray<T>;
 var
-  I: Int32;
+  LI, LLen, LDone: Int32;
+  LObj: TObject;
 begin
   if (AData = nil) or (System.Length(AData) = 0) then
     Exit(nil);
-  System.SetLength(Result, System.Length(AData));
-  for I := 0 to System.High(AData) do
-    Result[I] := ACloneFunc(AData[I]);
+  LLen := System.Length(AData);
+  System.SetLength(Result, LLen);
+  LDone := 0;
+  try
+    for LI := 0 to LLen - 1 do
+    begin
+      Result[LI] := ACloneFunc(AData[LI]);
+      Inc(LDone);
+    end;
+  except
+    // On failure, free any successfully cloned objects to prevent leaks
+    if GetTypeKind(TypeInfo(T)) = tkClass then
+      for LI := 0 to LDone - 1 do
+      begin
+        LObj := TObject(PPointer(@Result[LI])^);
+        if Assigned(LObj) then
+          LObj.Free;
+      end;
+    if LDone > 0 then
+      Fill<T>(Result, 0, LDone, Default(T));
+    raise;
+  end;
 end;
-
-(*
-class function TArrayUtilities.Contains<T>(const AData: TCryptoLibGenericArray<T>;
-  const AValue: T; const AComparer: IEqualityComparer<T>): Boolean;
-var
-  I: Int32;
-  LComparer: IEqualityComparer<T>;
-begin
-  if (AData = nil) or (System.Length(AData) = 0) then
-    Exit(False);
-  if AComparer = nil then
-    LComparer := TEqualityComparer<T>.Default
-  else
-    LComparer := AComparer;
-  for I := System.Low(AData) to System.High(AData) do
-    if LComparer.Equals(AData[I], AValue) then
-      Exit(True);
-  Result := False;
-end; *)
 
 class function TArrayUtilities.Contains(const AData: TCryptoLibInt32Array;
   const AValue: Int32): Boolean;
 var
-  I: Int32;
+  LI: Int32;
 begin
-  if (AData = nil) or (System.Length(AData) = 0) then
-    Exit(False);
-  for I := System.Low(AData) to System.High(AData) do
-    if AData[I] = AValue then
+  for LI := System.Low(AData) to System.High(AData) do
+    if AData[LI] = AValue then
       Exit(True);
   Result := False;
 end;
@@ -476,34 +413,31 @@ end;
 class function TArrayUtilities.Map<T, TResult>(const AData: TCryptoLibGenericArray<T>;
   const AFunc: TFunc<T, TResult>): TCryptoLibGenericArray<TResult>;
 var
-  I, LCount: Int32;
+  LI, LCount: Int32;
 begin
   LCount := System.Length(AData);
   System.SetLength(Result, LCount);
-  for I := 0 to LCount - 1 do
-    Result[I] := AFunc(AData[I]);
+  for LI := 0 to LCount - 1 do
+    Result[LI] := AFunc(AData[LI]);
 end;
 
 class function TArrayUtilities.ToString<T>(const AData: TCryptoLibGenericArray<T>;
   const AConverter: TFunc<T, String>): String;
 var
-  I, LCount: Int32;
+  LI, LCount: Int32;
   LSB: TStringBuilder;
 begin
   LCount := System.Length(AData);
   if LCount = 0 then
-  begin
-    Result := '[]';
-    Exit;
-  end;
+    Exit('[]');
   LSB := TStringBuilder.Create;
   try
     LSB.Append('[');
     LSB.Append(AConverter(AData[0]));
-    for I := 1 to LCount - 1 do
+    for LI := 1 to LCount - 1 do
     begin
       LSB.Append(', ');
-      LSB.Append(AConverter(AData[I]));
+      LSB.Append(AConverter(AData[LI]));
     end;
     LSB.Append(']');
     Result := LSB.ToString;
@@ -520,8 +454,7 @@ end;
 class procedure TArrayUtilities.ReverseInPlace<T>(var AArray: TCryptoLibGenericArray<T>;
   AFromIndex, AToIndex: Int32);
 var
-  LLeft: Int32;
-  LRight: Int32;
+  LLeft, LRight: Int32;
   LTemp: T;
 begin
   if (AFromIndex < 0) or (AToIndex > System.Length(AArray)) or (AFromIndex > AToIndex) then
--- CryptoLib/src/GeneralUtilities/ClpEnumUtilities.pas
+++ CryptoLib/src/GeneralUtilities/ClpEnumUtilities.pas
@@ -30,7 +30,7 @@ uses
 
 type
   /// <summary>
-  /// Utility class for enum operations
+  /// Utility class for enum operations.
   /// Works with ordinals; callers cast to their enum type e.g. TMyEnum(ordinal).
   /// </summary>
   TEnumUtilities = class sealed(TObject)
@@ -40,60 +40,66 @@ type
     /// <summary>
     /// Returns an array of ordinals for all defined values of the enum.
     /// For enums with gaps, only ordinals that have a name are included.
+    /// Note: iterates MinValue..MaxValue calling GetEnumName, so enums with
+    /// large gaps between values will be slower.
     /// Caller casts: TMyEnum(GetEnumValues(TypeInfo(TMyEnum))[i]).
     /// </summary>
     class function GetEnumValues(ATypeInfo: PTypeInfo): TCryptoLibInt32Array; overload; static;
+
     /// <summary>
     /// Returns an arbitrary (pseudo-random) ordinal of the enum, based on
     /// CurrentUnixMs. If the enum has no values, returns 0.
     /// Caller casts: TMyEnum(GetArbitraryValue(TypeInfo(TMyEnum))).
     /// </summary>
     class function GetArbitraryValue(ATypeInfo: PTypeInfo): Int32; overload; static;
+
     /// <summary>
     /// Tries to parse a string as an enum ordinal. Only parses single named
     /// constants: non-empty, first character a letter, no comma. When AReplacer
     /// is nil, the input is normalized by replacing '-' and '/' with '_'
-    /// before parsing; when AReplacer is assigned, it is applied to the input and the
-    /// result is used. Returns True and the ordinal in AResult when successful;
-    /// otherwise False and AResult is undefined. Caller casts: TMyEnum(AResult).
+    /// before parsing; when AReplacer is assigned, it is applied to the input.
+    /// Returns True and the ordinal in AResult when successful; otherwise
+    /// False and AResult is 0.
     /// </summary>
-    /// <param name="AReplacer">Optional. When nil, default normalization is used; when assigned, AReplacer(AInput) is used as the string to parse.</param>
-    class function TryGetEnumValue(ATypeInfo: PTypeInfo; const AInput: String; out AResult: Int32;
+    class function TryGetEnumValue(ATypeInfo: PTypeInfo; const AInput: String;
+      out AResult: Int32;
       const AReplacer: TFunc<String, String> = nil): Boolean; overload; static;
+
     /// <summary>
     /// Converts an enum ordinal to its declared name string.
-    /// Returns the result of GetEnumName(ATypeInfo, AOrdinal). Empty string if no name.
+    /// Returns empty string if ATypeInfo is nil, not an enum, or ordinal has no name.
     /// </summary>
-    class function ToString(ATypeInfo: PTypeInfo; AOrdinal: Int32): String; reintroduce; overload; static;
+    class function ToString(ATypeInfo: PTypeInfo; AOrdinal: Int32): String;
+      reintroduce; overload; static;
 
     // Generic overloads (T must be an enum); delegate to PTypeInfo versions.
 
     /// <summary>
-    /// Returns an array of all defined values of the enum. Delegates to
-    /// GetEnumValues(PTypeInfo).
+    /// Returns an array of all defined values of the enum.
     /// </summary>
     class function GetEnumValues<T>: TCryptoLibGenericArray<T>; overload; static;
+
     /// <summary>
-    /// Returns an arbitrary (pseudo-random) value of the enum. Delegates to
-    /// GetArbitraryValue(PTypeInfo). If the enum has no values, returns Default(T).
+    /// Returns an arbitrary (pseudo-random) value of the enum.
+    /// If the enum has no values, returns Default(T).
     /// </summary>
     class function GetArbitraryValue<T>: T; overload; static;
+
     /// <summary>
     /// Tries to parse a string as an enum value. When AReplacer is nil, default
-    /// normalization ( '-' and '/' to '_' ) is used; when assigned,
-    /// AReplacer(AInput) is used. Delegates to TryGetEnumValue(PTypeInfo, AInput, out Int32, AReplacer).
+    /// normalization ('-', '/' to '_') is used.
     /// On failure, AResult is Default(T).
     /// </summary>
-    /// <param name="AReplacer">Optional. When nil, default normalization is used; when assigned, AReplacer(AInput) is used as the string to parse.</param>
     class function TryGetEnumValue<T>(const AInput: String; out AResult: T;
-      const AReplacer: TFunc<string, string> = nil): Boolean; overload; static;
+      const AReplacer: TFunc<String, String> = nil): Boolean; overload; static;
+
     /// <summary>
     /// Tries to interpret an ordinal as a valid named value of the enum.
-    /// Uses the same validity rule as ToString (ordinal has a name). On success
-    /// sets AResult to T(AOrdinal) and returns True; otherwise AResult is
-    /// Default(T) and returns False.
+    /// On success sets AResult to T(AOrdinal) and returns True;
+    /// otherwise AResult is Default(T) and returns False.
     /// </summary>
     class function TryGetEnumFromOrdinal<T>(AOrdinal: Int32; out AResult: T): Boolean; static;
+
     /// <summary>
     /// Converts an enum value to its declared name string.
     /// </summary>
@@ -106,101 +112,93 @@ implementation
 
 class function TEnumUtilities.DefaultReplacer(const AInput: String): String;
 begin
-  Result := StringReplace(AInput, '-', '_', [rfReplaceAll, rfIgnoreCase]);
-  Result := StringReplace(Result, '/', '_', [rfReplaceAll, rfIgnoreCase]);
+  Result := StringReplace(AInput, '-', '_', [rfReplaceAll]);
+  Result := StringReplace(Result, '/', '_', [rfReplaceAll]);
 end;
 
 class function TEnumUtilities.GetEnumValues(ATypeInfo: PTypeInfo): TCryptoLibInt32Array;
 var
   LTypeData: PTypeData;
-  I, LOrd: Int32;
-  LList: TCryptoLibInt32Array;
-  LCount: Int32;
+  LI, LCount: Int32;
 begin
   if (ATypeInfo = nil) or (ATypeInfo^.Kind <> tkEnumeration) then
   begin
-    SetLength(Result, 0);
+    Result := nil;
     Exit;
   end;
+
   LTypeData := GetTypeData(ATypeInfo);
+  SetLength(Result, LTypeData^.MaxValue - LTypeData^.MinValue + 1);
   LCount := 0;
-  SetLength(LList, LTypeData^.MaxValue - LTypeData^.MinValue + 1);
-  for I := LTypeData^.MinValue to LTypeData^.MaxValue do
-  begin
-    if GetEnumName(ATypeInfo, I) <> '' then
+  for LI := LTypeData^.MinValue to LTypeData^.MaxValue do
+    if GetEnumName(ATypeInfo, LI) <> '' then
     begin
-      LList[LCount] := I;
+      Result[LCount] := LI;
       Inc(LCount);
     end;
-  end;
   SetLength(Result, LCount);
-  for LOrd := 0 to LCount - 1 do
-    Result[LOrd] := LList[LOrd];
 end;
 
 class function TEnumUtilities.GetArbitraryValue(ATypeInfo: PTypeInfo): Int32;
 var
   LValues: TCryptoLibInt32Array;
-  LPos: Int32;
   LMs: Int64;
 begin
   LValues := GetEnumValues(ATypeInfo);
   if System.Length(LValues) = 0 then
-  begin
-    Result := 0;
-    Exit;
-  end;
+    Exit(0);
   LMs := TDateTimeUtilities.CurrentUnixMs() and Int64($7FFFFFFF);
-  LPos := Int32(LMs mod System.Length(LValues));
-  Result := LValues[LPos];
+  Result := LValues[Int32(LMs mod System.Length(LValues))];
 end;
 
-class function TEnumUtilities.TryGetEnumValue(ATypeInfo: PTypeInfo; const AInput: String;
-  out AResult: Int32; const AReplacer: TFunc<String, String>): Boolean;
+class function TEnumUtilities.TryGetEnumValue(ATypeInfo: PTypeInfo;
+  const AInput: String; out AResult: Int32;
+  const AReplacer: TFunc<String, String>): Boolean;
 var
   LProcessed: String;
   LOrd: Int32;
 begin
   AResult := 0;
+
   if (ATypeInfo = nil) or (ATypeInfo^.Kind <> tkEnumeration) then
-  begin
-    Result := False;
-    Exit;
-  end;
+    Exit(False);
+
   // Only parse single named constants: non-empty, first char a letter, no comma
   if (System.Length(AInput) = 0) or (TStringUtilities.IndexOf(AInput, ',') > 0) then
-  begin
-    Result := False;
-    Exit;
-  end;
+    Exit(False);
+
   if not CharInSet(AInput[1], ['A'..'Z', 'a'..'z']) then
-  begin
-    Result := False;
-    Exit;
-  end;
+    Exit(False);
+
   if Assigned(AReplacer) then
     LProcessed := AReplacer(AInput)
   else
     LProcessed := DefaultReplacer(AInput);
+
   LOrd := GetEnumValue(ATypeInfo, LProcessed);
-  if LOrd >= 0 then
-  begin
-    AResult := LOrd;
-    Result := True;
-  end
-  else
-    Result := False;
+  if LOrd < 0 then
+    Exit(False);
+
+  AResult := LOrd;
+  Result := True;
+end;
+
+class function TEnumUtilities.ToString(ATypeInfo: PTypeInfo; AOrdinal: Int32): String;
+begin
+  if (ATypeInfo = nil) or (ATypeInfo^.Kind <> tkEnumeration) then
+    Exit('');
+  Result := GetEnumName(ATypeInfo, AOrdinal);
 end;
 
 class function TEnumUtilities.GetEnumValues<T>: TCryptoLibGenericArray<T>;
 var
   LOrds: TCryptoLibInt32Array;
-  I: Int32;
+  LI: Int32;
 begin
   LOrds := GetEnumValues(TypeInfo(T));
   SetLength(Result, System.Length(LOrds));
-  for I := 0 to System.High(LOrds) do
-    Move(LOrds[I], Result[I], SizeOf(T));
+  for LI := 0 to System.High(LOrds) do
+    Move(LOrds[LI], Result[LI], SizeOf(T));
 end;
 
 class function TEnumUtilities.GetArbitraryValue<T>: T;
@@ -212,7 +210,7 @@ begin
 end;
 
 class function TEnumUtilities.TryGetEnumValue<T>(const AInput: String;
-  out AResult: T; const AReplacer: TFunc<string, string>): Boolean;
+  out AResult: T; const AReplacer: TFunc<String, String>): Boolean;
 var
   LOrd: Int32;
 begin
@@ -226,7 +224,7 @@ end;
 class function TEnumUtilities.TryGetEnumFromOrdinal<T>(AOrdinal: Int32;
   out AResult: T): Boolean;
 begin
-  // We use `ToString` (which calls `GetEnumName`) to validate the ordinal rather than a simple Min/Max Value
+  // We use GetEnumName to validate the ordinal rather than a simple MinValue/MaxValue
   // range check because Delphi supports non-contiguous enums (e.g., A = 0, B = 5, C = 10).
   // GetEnumName returns an empty string for ordinals that fall in gaps between valid values,
   // correctly rejecting them, whereas a range check would incorrectly accept them.
@@ -237,18 +235,11 @@ begin
     AResult := Default(T);
 end;
 
-class function TEnumUtilities.ToString(ATypeInfo: PTypeInfo; AOrdinal: Int32): String;
-begin
-  Result := '';
-  if (ATypeInfo = nil) or (ATypeInfo^.Kind <> tkEnumeration) then
-    Exit;
-  Result := GetEnumName(ATypeInfo, AOrdinal);
-end;
-
 class function TEnumUtilities.ToString<T>(const AValue: T): String;
 var
-  LOrd: Byte;
+  LOrd: Int32;
 begin
+  LOrd := 0;
   Move(AValue, LOrd, SizeOf(T));
   Result := ToString(TypeInfo(T), LOrd);
 end;
